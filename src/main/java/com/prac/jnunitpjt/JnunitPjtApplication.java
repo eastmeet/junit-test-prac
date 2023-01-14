1fa4599c528e78d1e46bbf582c0e133958e02fca
eastmeet <ys932184@gmail.com>
2023-01-15 01:38:41 +0900

    initail commit

---
+++ src/main/java/com/prac/jnunitpjt/JnunitPjtApplication.java
@@ -0,0 +1,13 @@
+package com.prac.jnunitpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnunitPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnunitPjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prac/jnunitpjt/JnunitPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.jnunitpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnunitPjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
